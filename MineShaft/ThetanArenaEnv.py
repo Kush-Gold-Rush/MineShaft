995979d9cb2be489e184fc408c8aa189e5a13f4b
210388248 <110087604+210388248@users.noreply.github.com>
2022-08-02 14:23:59 +0800

    Update Capture and frame-rate
    Capture and return the screen in format of numpy.array 
    The frame-rate of screen capture better be more than 30 frames-per-second (optional)

--- MineShaft/ThetanArenaEnv.py
+++ MineShaft/ThetanArenaEnv.py
@@ -18,7 +18,43 @@ class ThetanArenaEnv(BaseEnv):
         pass
     
     def _screen_cap(self):
-        pass
+        """
+        This is the funtion of screen cap 
+        First you need to the below module before you run the code
+        The code will keep capture the screen
+        It can auto get the resolution of the screen
+        Capture and return the screen in format of numpy.array
+        And the frame-rate of screen capture better be more than 30 frames-per-second 
+        """
+        import cv2
+        import mss
+        import numpy
+        import pyautogui
+
+        with mss.mss() as sct:
+            #It can get the size the screen and take the value for the resolution
+            x,y=pyautogui.size()
+            # The screen part to capture
+            monitor = {"top": 0, "left": 0, "width": x, "height": y}
+
+            while "Screen capturing":
+              # Get raw pixels from the screen, save it to a Numpy array
+              img = numpy.array(sct.grab(monitor))
+
+              # Display the picture
+              cv2.imshow("OpenCV/Numpy normal", img)
+
+              # Display the picture in grayscale
+              # cv2.imshow('OpenCV/Numpy grayscale',
+              #            cv2.cvtColor(img, cv2.COLOR_BGRA2GRAY))
+
+              # print in 30 fps
+              print("fps: {}".format(30))
+
+              # Press "q" to quit
+              if cv2.waitKey(25) & 0xFF == ord("q"):
+                   cv2.destroyAllWindows()
+                   break
     
     def _keyboard_input(self, action):
         # scan and find keyboard action
