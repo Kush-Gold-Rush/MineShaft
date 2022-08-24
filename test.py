78e407de91266d8c50f24b33c89c92866e426c27
Stupid <22520563+NewJerseyStyle@users.noreply.github.com>
2022-08-24 16:14:58 +0800

    bugfix integration baseline3 env init

--- test.py
+++ test.py
@@ -1,22 +1,32 @@
+import time
+
 from stable_baselines3 import PPO
 
 from MineShaft import ThetanArenaEnv
 
 def main():
 	env = ThetanArenaEnv()
-	env.enter_match()
-	# env.enter_match(random_character=True)
 	model = PPO("MlpPolicy", env, verbose=1)
 	model.learn(total_timesteps=10_000)
 
 	info = {'waiting': True}
 	action = env.action_space.sample()
-	for _ in range(1000):
+	action = action * 0
+	i = 0
+	while i < 10_000:
+		start_time = time.time()
 		if not info['waiting']:
 			action, _states = model.predict(observation, deterministic=True)
+		else:
+			i -= 1
 		observation, reward, done, info = env.step(action)
 		if done:
 			observation = env.reset()
+		if time.time() - start_time < 0.9:
+			try:
+				time.sleep(0.1 - time.time() - start_time)
+			except:
+				pass
 
 	env.close()
 
